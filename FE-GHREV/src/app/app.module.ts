f614e7c88ec4457b4ddd5fe734f90141eb73c023
GiampyLux <giampaolopaladino01@gmail.com>
2024-09-21 12:22:27 +0200

    Aggiunto bs

--- FE-GHREV/src/app/app.module.ts
+++ FE-GHREV/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
